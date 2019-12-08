ca47a35442215841bc39c0a334cbde7ac51ac95d
3kesyuri <1989sintetsu@gmail.com>
2019-12-08 14:23:36 +0900

    ルーティング設定&Carrierwaveを導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,20 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    @message = Message.new
+    @messages = Message.create(message_params)
+    if @messages.save
+      redirect_to root_path, "メッセージを投稿しました"
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:message, :image)
+  end
 end
